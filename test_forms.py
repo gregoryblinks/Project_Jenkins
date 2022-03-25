3f1d2e85bd745c649ae9ec6151760e6131535fee
gregoryblinks <ohwerhigregory@gmail.com>
2022-03-25 08:56:29 +0100

    First commit

--- test_forms.py
+++ test_forms.py
@@ -1,15 +1,39 @@
+from flask_login import current_user
+from pathlib import Path
+from flask_login import UserMixin
 import pytest
 from urllib import response
 from flaskblog import app
 import unittest
 
+from flaskblog.models import User
+
 class FlaskTestCase(unittest.TestCase):
+    
     def test_startpage(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
     
+    def test_user_registeration(self):
+            tester = app.test_client(self)
+            response = tester.post('register/', data=dict(
+                username='Michael', email='michael@realpython.com',
+                password='python', confirm='python'
+            ), follow_redirects=True)
+            self.assertEqual(response.status_code 200)
+    
+    def test_user_registeration_failed(self):
+            tester = app.test_client(self)
+            response = tester.post('register/', data=dict(
+                username='Michael', email='michael@realpython.com',
+                password='python', confirm='password'
+            ), follow_redirects=False)
 
+            
+
+
+    
 
 if __name__ == '__main__':
     unittest.main()
